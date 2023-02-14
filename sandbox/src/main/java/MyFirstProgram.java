891e502f6e69c7fc66b7f77f6e42a42bbf42ea23
Veronika <golikova.veronika.igorevna@mail.ru>
2023-02-14 20:35:13 +0300

    Add function in MyFirstProgram class for counting distance between points.

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -6,6 +6,8 @@ public class MyFirstProgram {
         Point p1 = new Point(2.0, 5.0);
         Point p2 = new Point(6.0, 10.0);
 
+        System.out.println(Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y)));
+
         Point.countDistance(p1, p2);
     }
 }
