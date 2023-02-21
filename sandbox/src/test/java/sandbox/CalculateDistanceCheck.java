1052731fbd0b4edf45c121fac04a9b742c6b8e92
Veronika <golikova.veronika.igorevna@mail.ru>
2023-02-21 13:24:33 +0300

    Add more tests for ContactCreationForm.

--- sandbox/src/test/java/sandbox/CalculateDistanceCheck.java
+++ sandbox/src/test/java/sandbox/CalculateDistanceCheck.java
@@ -6,10 +6,30 @@ import org.testng.annotations.Test;
 public class CalculateDistanceCheck {
 
     @Test
-    public void testDistanceCheck() {
+    public void testDistanceCheckWithPositiveValue() {
         Point p1 = new Point(3, 4);
         Point p2 = new Point(3, 6);
         Assert.assertEquals(p1.countDistance(p2), 2.0);
+    }
 
+    @Test
+    public void testDistanceCheckWithNegativeValue() {
+        Point p1 = new Point(-4, 4);
+        Point p2 = new Point(3, 6);
+        Assert.assertEquals(p1.countDistance(p2), 7.280109889280518);
+    }
+
+    @Test
+    public void testDistanceCheckWithZeroValue() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3, 6);
+        Assert.assertEquals(p1.countDistance(p2), 6.708203932499369);
+    }
+
+    @Test
+    public void testDistanceCheckWithSimillarPoint() {
+        Point p1 = new Point(2, 2);
+        Point p2 = new Point(2, 2);
+        Assert.assertEquals(p1.countDistance(p2), 0.0);
     }
 }
