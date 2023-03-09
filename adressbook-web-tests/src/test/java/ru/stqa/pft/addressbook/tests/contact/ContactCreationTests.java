7c09253e48e29ef00273010e97bd971a4a62620c
Veronika <golikova.veronika.igorevna@mail.ru>
2023-03-09 14:50:48 +0300

    Update ContactCreationTests and ContactDeletionTests by assertion by collections.

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/contact/ContactCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/contact/ContactCreationTests.java
@@ -1,16 +1,34 @@
 package ru.stqa.pft.addressbook.tests.contact;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.tests.TestBase;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
   @Test
   public void testCreateNewContact() throws Exception {
-    app.getNavigationHelper().gotoAddNewContactPage();
-    app.getContactHelper().createNewContact(new ContactData("Veronika", "Igorevna", "Golikova", "nick", "some@mail.ru"));
     app.getNavigationHelper().gotoHomePage();
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getNavigationHelper().gotoAddNewContactPage();
+    ContactData contact = new ContactData("Veronika", "Igorevna", "Golikova", "nick", "some@mail.ru");
+    app.getContactHelper().createNewContact(contact);
+    app.getNavigationHelper().gotoHomePage();
+
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size()+1);
+
+    before.add(contact);
+    Comparator<? super ContactData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
+
+
 
   }
 
--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/contact/ContactDeletionTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/contact/ContactDeletionTests.java
@@ -1,8 +1,12 @@
 package ru.stqa.pft.addressbook.tests.contact;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.tests.TestBase;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactDeletionTests extends TestBase {
 
@@ -14,11 +18,20 @@ public class ContactDeletionTests extends TestBase {
             app.getContactHelper().createNewContact(new ContactData("VeronikaDel", "IgorevnaDel", "GolikovaDel", "nickDel", "someDel@mail.ru"));
             app.getNavigationHelper().gotoHomePage();
         }
-        //TODO change index -> before.size() - 1
-        app.getContactHelper().selectContact(1);
+        List<ContactData> before = app.getContactHelper().getContactList();
+        app.getContactHelper().selectContact(before.size() - 1);
         app.getContactHelper().deleteSelectedContact();
         app.getContactHelper().submitContactDeletion();
         app.getNavigationHelper().gotoHomePage();
+
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size()-1);
+
+        before.remove(before.size()-1);
+        Comparator<? super ContactData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
 
     }
 }
