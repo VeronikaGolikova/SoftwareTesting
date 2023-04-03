60e3517a087788ef9adde5858f80d9464456305d
Veronika <golikova.veronika.igorevna@mail.ru>
2023-04-03 22:31:01 +0300

    Add new module mantis-tests.

---
+++ mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,58 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    private final Properties properties;
+    WebDriver wd;
+
+    public Browser browser;
+
+    public ApplicationManager(Browser browser) {
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        if (browser.equals(Browser.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser .equals(Browser.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser .equals(Browser.SAFARI)) {
+            wd = new SafariDriver();
+        }
+
+        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
+    }
+
+
+    public void stop() {
+        wd.quit();
+    }
+
+    private boolean isElementPresent(By by) {
+      try {
+        wd.findElement(by);
+        return true;
+      } catch (NoSuchElementException e) {
+        return false;
+      }
+    }
+}
---
+++ mantis-tests/src/main/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,35 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.Browser;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+public class TestBase {
+
+    protected final static ApplicationManager app = new ApplicationManager(new Browser() {
+        @Override
+        public String browserName() {
+            return System.getProperty("browser", Browser.FIREFOX.browserName());
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(obj instanceof Browser) {
+                return this.browserName().equals(((Browser)obj).browserName());
+            } else
+                return false;
+        }
+    });
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+
+}
