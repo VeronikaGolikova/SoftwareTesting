b70a83f5a2b5c391786d2513e33d3a3f209b4ea3
Veronika <golikova.veronika.igorevna@mail.ru>
2023-04-15 14:44:25 +0300

    Fix strBrowser value.

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -32,7 +32,7 @@ public class ApplicationManager {
     public ApplicationManager(Browser browser) {
         this.browser = browser;
         properties = new Properties();
-        strBrowser = browser.toString();
+        strBrowser = browser.browserName();
     }
 
 
@@ -41,7 +41,6 @@ public class ApplicationManager {
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
         dbHelper = new DbHelper();
-System.out.println("!!!!!!!!!!!!!!!!!!");
         if ("".equals(properties.getProperty("selenium.server"))) {
             if (browser.equals(Browser.FIREFOX)) {
                 wd = new FirefoxDriver();
